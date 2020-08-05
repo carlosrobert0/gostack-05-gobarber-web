755b1a011a11136ec1e1bd317f22ca4ca1abb9c9
Carlos Roberto <carlosroberto15755@gmail.com>
2020-08-05 20:31:36 -0300

    Login pelo contexto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 
-import AuthContext from '../../context/AuthContext'
+import { AuthContext } from '../../context/AuthContext'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -15,12 +15,17 @@ import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { name } = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext)
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({})
 
@@ -32,12 +37,17 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false
       })
+
+      signIn({
+        email: data.email,
+        password: data.password
+      })
     } catch (err) {
       const errors = getValidationErrors(err)
 
       formRef.current?.setErrors(errors)
     }
-  }, [])
+  }, [signIn])
 
   return (
     <Container>
