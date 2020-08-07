cea89b591afdaa67da34fddb6c92da070e91b2b2
Carlos Roberto <carlosroberto15755@gmail.com>
2020-08-07 02:25:09 -0300

    Adicionando e removendo Toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -51,7 +51,11 @@ const SignIn: React.FC = () => {
         formRef.current?.setErrors(errors)
       }
 
-      addToast()
+      addToast({
+        type: 'success',
+        title: 'Erro na autenticacao',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
+      })
     }
   }, [signIn, addToast])
 
