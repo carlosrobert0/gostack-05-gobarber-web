d678ea2e9d054429c2e5e27ca2fdd5baa0ed77ac
Carlos Roberto <carlosroberto15755@gmail.com>
2020-08-07 20:18:12 -0300

    Animando Toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -52,7 +52,7 @@ const SignIn: React.FC = () => {
       }
 
       addToast({
-        type: 'success',
+        type: 'error',
         title: 'Erro na autenticacao',
         description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
       })
