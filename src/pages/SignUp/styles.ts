2d216756724a40cbdf86a29c24bab32cf3acfeb8
Carlos Roberto <carlosroberto15755@gmail.com>
2020-08-07 21:29:36 -0300

    Configurando rotas

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { shade } from 'polished'
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png'
@@ -13,13 +13,30 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-
   align-items: center;
-
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     margin: 80px 0;
@@ -44,7 +61,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #F4EDE8;
+    color: #FF9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -58,7 +75,7 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#F4EDE8')};
+      color: ${shade(0.2, '#FF9000')};
     }
   }
 `
