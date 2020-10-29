422dd6e90996902784787658d1e298e053db9eae
Carlos Roberto <carlosroberto15755@gmail.com>
2020-10-29 09:38:30 -0300

    Ajustes reset_password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,7 +16,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
     <Route path="/forgot-password" component={ForgotPassword} />
-    <Route path="/reset-password" component={ResetPassword} />
+    <Route path="/reset_password" component={ResetPassword} />
 
     <Route path="/profile" component={Profile} isPrivate/>
     <Route path="/dashboard" component={Dashboard} isPrivate/>
