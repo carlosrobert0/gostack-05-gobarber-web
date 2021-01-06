797e650d6817b175acabac67f2559225b41f1909
carlosrobert0 <carlosroberto15755@gmail.com>
2021-01-06 17:37:27 -0300

    App

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -98,7 +98,7 @@ const Profile: React.FC = () => {
         description: 'Ocorreu um erro ao atualizar o perfil, tente novamente.'
       })
     }
-  }, [addToast, history])
+  }, [addToast, history, updateUser])
 
   const handleAvatarChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
